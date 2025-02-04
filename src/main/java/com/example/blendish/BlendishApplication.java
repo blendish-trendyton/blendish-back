c19354d9bd4ad28a864c7fcf49affeaa8b5e6ccc
dlqja <beomjin754@gmail.com>
2025-02-04 19:01:59 +0900

    [Chore] Initialize

---
+++ src/main/java/com/example/blendish/BlendishApplication.java
@@ -0,0 +1,13 @@
+package com.example.blendish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlendishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlendishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blendish/BlendishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blendish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlendishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

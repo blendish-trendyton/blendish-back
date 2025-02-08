228bef3216377ce5ac0c571acfc6c8d8e326f1d5
dlqja <beomjin754@gmail.com>
2025-02-09 01:33:49 +0900

    feat: Add Swagger API documentation#3

---
+++ src/main/java/com/example/blendish/global/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.blendish.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Blendish")
+                .description("나만의 레시피 만들기")
+                .version("1.0.0"); // API의 버전
+    }
+}

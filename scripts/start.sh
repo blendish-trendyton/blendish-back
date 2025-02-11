0f29bba9cd242af9c348592ca60a59fe855194fd
Junyeong-An <2117ab@gmail.com>
2025-02-11 23:04:45 +0900

    [Chore] 배포 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -25,8 +25,7 @@ echo "> 새 애플리케이션을 $IDLE_PROFILE 로 실행합니다."
 
 # 설정 파일의 위치를 지정하고 active profile을 통해 구동될 포트를 지정합니다.
 nohup java -jar \
--Dspring.config.location=$REPOSITORY/config/application.yml,\
-$REPOSITORY/config/application-prod.yml,\
-$REPOSITORY/config/application-$IDLE_PROFILE.yml \
--Dspring.profiles.active=$IDLE_PROFILE,prod \
-$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  -Dserver.port=$IDLE_PORT \
+  -Dspring.config.location="$REPOSITORY/config/application.yml,$REPOSITORY/config/application-prod.yml,$REPOSITORY/config/application-$IDLE_PROFILE.yml" \
+  -Dspring.profiles.active=$IDLE_PROFILE \
+  "$JAR_NAME" > "$LOG_FILE" 2>&1 &

d391a4f0933ee3ff1aaec1f37152a6bedb2f0132
Junyeong-An <2117ab@gmail.com>
2025-02-17 01:53:05 +0900

    [Refactor] Recipe 생성 로직 변경

--- src/main/java/com/example/blendish/controller/RecipeController.java
+++ src/main/java/com/example/blendish/controller/RecipeController.java
@@ -1,6 +1,6 @@
 package com.example.blendish.controller;
 
-import com.example.blendish.domain.recipe.dto.AddRecipeDTO;
+import com.example.blendish.domain.recipe.dto.AddUserRecipeDTO;
 import com.example.blendish.domain.recipe.service.RecipeService;
 import com.example.blendish.global.dto.ApiResponseTemplate;
 import com.example.blendish.global.response.SuccessCode;
@@ -22,7 +22,7 @@ public class RecipeController implements RecipeSwagger{
     private final S3UploadService s3UploadService;
 
     @PostMapping(consumes = {"multipart/form-data"})
-    public ResponseEntity<ApiResponseTemplate<String>> addRecipe(@RequestBody AddRecipeDTO addRecipeDTO,
+    public ResponseEntity<ApiResponseTemplate<String>> addRecipe(@RequestBody AddUserRecipeDTO addRecipeDTO,
                                                                  @RequestPart(value = "image", required = false) MultipartFile image,
                                                                  @AuthenticationPrincipal UserDetails userDetails) throws IOException {
         String imageUrl = (image != null && !image.isEmpty()) ? s3UploadService.saveFile(image) : null;
